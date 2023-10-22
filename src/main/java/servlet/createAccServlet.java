64225dea084498e8d8dd2927f667caafa013bb07
P. Sehan Devinda <sehandevinda1@gmail.com>
2023-10-22 13:39:59 +0530

    2023-10-22(1.39pm)

--- src/main/java/servlet/createAccServlet.java
+++ src/main/java/servlet/createAccServlet.java
@@ -13,7 +13,7 @@ import util.customerDBUtil;
 
 
 
-@WebServlet("/customerInsert")
+@WebServlet("/createAccServlet")
 public class createAccServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
